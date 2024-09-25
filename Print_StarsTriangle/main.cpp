1a9b089ec780a8971e0aa44329e118f4925802aa
Sonia <30045332@cc.peralta.edu>
2024-09-25 01:57:17 -0700

    Initial Commit

---
+++ Print_StarsTriangle/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  Print_StarsTriangle
+//
+//  Created by Sonia Lopchan on 9/25/24.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<=i; j++){
+            cout << "*";
+    }
+        cout <<endl;
+}
+    
+    return 0;
+}
